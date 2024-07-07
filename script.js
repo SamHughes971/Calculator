b67cf81f67e27f54c9efc2fbad3f5a460d17e76f
Samuel Hughes <samuel971@hotmail.co.uk>
2024-07-07 17:00:05 +0100

    Create subtract function and test

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function addition(numOne, numTwo){
     return numOne + numTwo;
 }
 
+function subtract(numOne, numTwo){
+    return numOne - numTwo
+}
+
 const numOne = 8
 const numTwo = 3
 
 console.log(addition(numOne, numTwo));
+console.log(subtract(numOne, numTwo));
