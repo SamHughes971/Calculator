95eb73bd2302a76aecf79f2c6c43df27f28275c9
Samuel Hughes <samuel971@hotmail.co.uk>
2024-07-08 11:29:24 +0100

    Create multiply and divide functions

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ function addition(numOne, numTwo){
 function subtract(numOne, numTwo){
     return numOne - numTwo
 }
+function multiply(numOne, numTwo){
+    return numOne * numTwo
+}
+function divide(numOne, numTwo){
+    return numOne / numTwo
+}
 
 const numOne = 8
 const numTwo = 3
 
 console.log(addition(numOne, numTwo));
 console.log(subtract(numOne, numTwo));
+console.log(multiply(numOne, numTwo));
+console.log(divide(numOne, numTwo));
